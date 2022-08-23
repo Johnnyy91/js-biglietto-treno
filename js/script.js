6627925d90d54077b435228217aee37921ddfc14
Johnnyy91 <claudiosardilli@gmail.com>
2022-08-23 17:09:12 +0200

    file creation

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log('JS OK')
+
+const text = prompt('Età passeggero?');
+const textAsNumber = parseInt(text);
+console.log(textAsNumber);
+
+
+const km = prompt ('km percorsi?')
+const number=parseInt(km);
+console.log(km);
+
+if ('under18') {
+    
+} else {
+    
+}
